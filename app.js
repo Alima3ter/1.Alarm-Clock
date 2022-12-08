091c8645abed64189750804537f352f066d24889
Ali Ma3ter <AliMa3ter@gmail.com>
2022-12-09 00:30:52 +0330

    Audio added css edited & js finished

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const selectMenu = document.querySelectorAll("select");
 const timeBox = document.querySelector(".time");
 const setAlarmBtn = document.querySelector("button");
+const content = document.querySelector(".content");
+
+let alarmTime,
+  alarmState = " noset";
+const ring = new Audio("./Media/mixkit-facility-alarm-sound-999.wav");
 
 for (let i = 23; i >= 0; i--) {
   i = i < 10 ? "0" + i : i;
@@ -24,4 +29,29 @@ setInterval(() => {
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
   timeBox.innerHTML = `${h}:${m}:${s}`;
+  if (alarmTime == `${h}:${m}`) {
+    ring.play();
+    ring.loop = true;
+  }
 });
+
+setAlarmBtn.addEventListener("click", () => {
+  alarmTime = `${selectMenu[0].value}:${selectMenu[1].value}`;
+  if (alarmTime.includes("Hour") || alarmTime.includes("Minute")) {
+    return alert("Select Alarm Time First!");
+  }
+  checkState(alarmState);
+});
+function checkState(state) {
+  if (state == "noset") {
+    content.classList.add("disable");
+    setAlarmBtn.innerText = "Clear Alarm!";
+    alarmState = "set";
+  } else {
+    content.classList.remove("disable");
+    alarmTime = "";
+    ring.pause();
+    alarmState = "noset";
+    setAlarmBtn.innerText = "Set Alarm";
+  }
+}
